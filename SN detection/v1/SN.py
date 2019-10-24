d13d5005a4ea34ab0cb2177d11d3c99a4891fb4a
David Campbell <d.a.campbell2@herts.ac.uk>
2019-10-24 16:03:55 +0100

    Added v2 (with C)

--- SN detection/v1/SN.py
+++ SN detection/v1/SN.py
@@ -6,25 +6,14 @@ from astropy.io import fits
 import sys
 from PIL import Image
 import math
-#import scipy.misc as misc
 
+#prefix of the input images
 name="M95"
 
+#width of the output image
+width=800
 
-pix=800
-
-#m82
-#cx=149.0
-#cy=69.7
-
-#nv
-#cx=305.9
-#cy=20.8
-
-#m95
-#cx=161
-#cy=11.7
-
+#binning ratios
 bin=np.zeros(shape=(5))
 
 bin[1]=0.36
@@ -32,6 +21,7 @@ bin[2]=0.36
 bin[3]=1.8
 bin[4]=3
 
+#open two fits to compare
 hdulist1 = fits.open(name+"-1.fit")
 hdulist2 = fits.open(name+"-2.fit")
 
@@ -41,20 +31,25 @@ header2=hdulist2[0].header
 exp1=header1['EXPTIME']/100
 exp2=header2['EXPTIME']/100
 
+
 pxsize1=(header1['XPIXSZ']/header1['XBINNING'])*bin[header1['XBINNING']]
 pxsize2=(header2['XPIXSZ']/header2['XBINNING'])*bin[header2['XBINNING']]
 
 scidata1 = hdulist1[0].data
 scidata2 = hdulist2[0].data
 
+#centre coordinates
 cra1=header1['CRVAL1']
 cdec1=header1['CRVAL2']
 
 cra2=header2['CRVAL1']
 cdec2=header2['CRVAL2']
 
+#average centre coordinate
 cx=(cra1+cra2)/2
 cy=(cdec1+cdec2)/2
+
+#pixel width of images
 
 xaxis1=header1['NAXIS1']
 yaxis1=header1['NAXIS2']
@@ -62,16 +57,19 @@ yaxis1=header1['NAXIS2']
 xaxis2=header2['NAXIS1']
 yaxis2=header2['NAXIS2']
 
+#plate scale of images
+
 fdec=math.cos(math.radians(cy))
 
 xscale1=header1['CDELT1']/fdec
 yscale1=header1['CDELT2']
 
-rot1=0-(header1['CROTA1']+header1['CROTA2'])/2
-
 xscale2=header2['CDELT1']/fdec
 yscale2=header2['CDELT2']
 
+#rotation of images
+
+rot1=0-(header1['CROTA1']+header1['CROTA2'])/2
 rot2=0-(header2['CROTA1']+header2['CROTA2'])/2
 
 print(rot1)
@@ -80,8 +78,12 @@ print(rot2)
 #print(scale1)
 #print(scale2)
 
+#scale of first image
 
 scaley=yscale1*yaxis1*0.5
+scalex=scaley/math.cos(math.radians(cy))
+
+#sine and cosine of position angles
 
 sinr1=math.sin(math.radians(rot1))
 cosr1=math.cos(math.radians(rot1))
@@ -89,7 +91,7 @@ cosr1=math.cos(math.radians(rot1))
 sinr2=math.sin(math.radians(rot2))
 cosr2=math.cos(math.radians(rot2))
 
-
+#offset of images
 
 cxd1=cx-cra1
 cyd1=cy-cdec1
@@ -103,21 +105,24 @@ print(cyd1)
 print(cyd2)
 
 
-
 #scaley=0.25/2
-scalex=scaley/math.cos(math.radians(cy))
-
 
 #scalex=scaley
 
-scaley2=scaley/pix
-scalex2=scalex/pix
+#scale of second image
 
-a1 = np.zeros(shape=(pix,pix))
-a2 = np.zeros(shape=(pix,pix))
+scaley2=scaley/width
+scalex2=scalex/width
 
-b = np.zeros(shape=(pix,pix))
-c = np.zeros(shape=(pix,pix))
+#create blank images
+
+a1 = np.zeros(shape=(width,width))
+a2 = np.zeros(shape=(width,width))
+
+b = np.zeros(shape=(width,width))
+c = np.zeros(shape=(width,width))
+
+#approximate background value
 
 bg1 = np.median(scidata1)*0.98
 bg2 = np.median(scidata2)*0.98
@@ -125,12 +130,12 @@ bg2 = np.median(scidata2)*0.98
 print (bg1)
 print (bg2)
 
-for (x,y), value in np.ndenumerate(b):
+#loop through the pixels (this is probably inefficient)
 
+for (x,y), value in np.ndenumerate(b):
 	
-	
-	xc=(x-(pix/2))*scalex2
-	yx=(y-(pix/2))*scaley2
+	xc=(x-(width/2))*scalex2
+	yx=(y-(width/2))*scaley2
 
 	x1=xc+cxd1		
 	y1=yx+cyd1
